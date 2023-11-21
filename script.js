2ae155de1b06b7b391e5412498b2fca963e64b99
chiararainaudi <crainaudi@itba.edu.ar>
2023-11-21 16:58:41 -0300

    ..

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-// Set dia 
+
     let countDownDate = new Date("Dec 25, 2023 15:37:25").getTime();
     console.log(countDownDate)
-    // Update contador
+  
     let x = setInterval(function() {
     
-      // dia de hoy
+      
     let now = new Date().getTime();
     
-      // dias que faltan 
+      
     let distance = countDownDate - now;
     
-      // calculos
+  
       let days = Math.floor(distance / (1000 * 60 * 60 * 24));
       let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       let seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-      // muestra los elementos with id="demo"
+ 
     document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
     
-      // cuando el contador termine
+
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("demo").innerHTML = "EXPIRED";
@@ -33,18 +33,18 @@
 
 
   function enviarFormulario() {
-    // Obtener los valores de los campos
+
     var nombre = document.getElementById("nombre").value;
     var email = document.getElementById("email").value;
     var mensaje = document.getElementById("mensaje").value;
 
-    // Limpiar mensajes de error previos
+
     document.getElementById("errorNombre").innerText = "";
     document.getElementById("errorEmail").innerText = "";
     document.getElementById("errorMensaje").innerText = "";
 
   
-      // Validar campos (puedes agregar más validaciones según tus necesidades)
+
       var valido = true;
   
       if (nombre === "") {
@@ -63,7 +63,6 @@
       }
   
       if (valido) {
-          // Aquí podrías realizar la lógica para enviar el formulario
           alert("Formulario enviado correctamente.");
       }
   }
